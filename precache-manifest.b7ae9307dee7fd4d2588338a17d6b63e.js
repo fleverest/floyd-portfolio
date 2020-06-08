6400e90ad5af552db176ac0d4016c87eb610590f
fleverest <41366210+fleverest@users.noreply.github.com>
2020-06-08 16:19:23 +1000

    Updates

---
+++ precache-manifest.b7ae9307dee7fd4d2588338a17d6b63e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67066dc347ba367fbf11d092ce29f6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b25936b6d41c29a7e0e5",
+    "url": "/static/css/main.ec5359d7.chunk.css"
+  },
+  {
+    "revision": "bd50b8e12a386ce26caf",
+    "url": "/static/js/2.64d56dc1.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.64d56dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25936b6d41c29a7e0e5",
+    "url": "/static/js/main.32b6548e.chunk.js"
+  },
+  {
+    "revision": "64ae5604892a3a202f84",
+    "url": "/static/js/runtime-main.e4e62d69.js"
+  },
+  {
+    "revision": "b3c318afe4ecb63e72a00e457121a1cb",
+    "url": "/static/media/back.b3c318af.jpeg"
+  },
+  {
+    "revision": "9271fa8b2ecdfb8f62fd955692bdaa1e",
+    "url": "/static/media/floyd.9271fa8b.jpg"
+  }
+]);
